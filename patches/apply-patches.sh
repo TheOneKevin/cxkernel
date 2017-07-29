57131ba02a53a95811b390e8f4461ac148552be0
Kevin <kevindai02@outlook.com>
2017-07-28 22:11:44 -0400

    Rewrite LiquiDOS v 0.02
    Start of the months long rewrite process

--- patches/apply-patches.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "===== Patching newlib now ====="
-echo "Removing previous versions of newlib..."
-rm -rf newlib/
-echo "Extracting newlib..."; tar -xzf newlib.tar.gz; echo "Patching newlib...";
-(cd newlib && patch -p 1 < ../newlib.patch)
