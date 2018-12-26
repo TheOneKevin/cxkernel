f99bf7adc7280dee5dbc5a435eec9d4154256dc1
Kevin <kevindai02@outlook.com>
2018-12-26 11:08:29 -0500

    I think I managed to get the preprocessor to work with Cmake. Thus this will be last autoconf commit.

---
+++ loader/x86/mmu.cc
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2018 The cxkernel Authors. All rights reserved.
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT
+ *
+ * @file   mmu.cc
+ * @author Kevin Dai \<kevindai02@outlook.com\>
+ * @date   Created on December 26 2018, 9:41 AM
+ */
+
+#include "include/global.h"
+
+
