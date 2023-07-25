c743b490a7566bf3df9ecf5b85ff86bf95d1d89d
TheOneKevin <kevindai02@outlook.com>
2023-07-25 17:56:56 -0400

    [lib/elf] Added ability to iterate through headers
    - Safe get_header function (very nice abstraction)
    - get_image() should be implemented in a similar fashion maybe?

--- lib/elf/elf.h
+++ lib/elf/elf.h
@@ -2,22 +2,36 @@
 
 #include "sysv_elf_abi.h"
 #include <ebl/status.h>
+#include <ebl/type_traits.h>
 
 namespace elf {
 
     class Context {
+    private:
+        struct Context32;
+        struct Context64;
+        template<typename U> struct header;
     public:
         static status_t load(Context& ctx, void* img);
         bool is_64bits() const { return is_64bits_; }
+        template<typename T> header<T> get_header() {
+            static_assert(ebl::always_false<T>::value,
+                "Unsupported type <T> for get_header()");
+        }
+        template<> header<elf64_phdr_t> get_header<elf64_phdr_t>() {
+            return header<elf64_phdr_t>{impl64_.phdr, impl64_.img->e_phnum};
+        }
+        // FIXME: Should this be genericized too?
+        void* get_image() const {
+            return is_64bits_ ? (void*) impl64_.img : (void*) impl32_.img;
+        }
     
-    private:
+    public:
         Context() {}
     
     private:
         struct Context32 {
-        public:
             status_t load(elf32_ehdr_t* hdr);
-        private:
             elf32_ehdr_t* img;
             elf32_shdr_t* shdr;
             elf32_phdr_t* phdr;
@@ -26,17 +40,37 @@ namespace elf {
         };
 
         struct Context64 {
-        public:
             status_t load(elf64_ehdr_t* hdr);
             elf64_shdr_t* find_section(const char* name);
             elf64_addr_t get_symbol_addr(elf64_addr_t addr);
             char* get_symbol_name(elf64_addr_t addr);
-        private:
             elf64_ehdr_t* img;
             elf64_shdr_t* shdr;
             elf64_phdr_t* phdr;
             elf64_shdr_t* symtab;
             elf64_shdr_t* strtab;
+        };
+    
+    private:
+        template<typename U>
+        struct header {
+        private:
+            struct iterator {
+                iterator(U* hdr) : hdr_{hdr} {}
+                iterator& operator++() { hdr_++; return *this; }
+                bool operator==(const iterator& a) const { return hdr_ == a.hdr_; }
+                bool operator!=(const iterator& a) const { return hdr_ != a.hdr_; }
+                U& operator*() const { return *hdr_; }
+            private:
+                U* hdr_;
+            };
+        public:
+            header(U* hdr, uint16_t limit) : hdr_{hdr}, limit_{limit} {}
+            iterator begin() const { return iterator{hdr_}; }
+            iterator end() const { return iterator{&hdr_[limit_]}; }
+        private:
+            U* hdr_;
+            uint16_t limit_;
         };
 
     private:
